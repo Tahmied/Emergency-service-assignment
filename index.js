f25d95c1fd555b9448393c89f40fcf66669c5534
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-08-27 16:38:48 +0600

    - added call buttion functionality which now shows the alert, and adds a li item in the call history list

--- index.js
+++ index.js
@@ -49,3 +49,55 @@ heartIcon.forEach((icon)=>{
     heartCountShow.innerText = heartCount
   })
 })
+
+// call button functionality
+
+function getCurrentTime(){
+  const now = new Date()
+  const time = now.toLocaleTimeString('en-US' , {
+    hour : 'numeric',
+    minute : '2-digit',
+    second : '2-digit',
+    hour12 : true
+  })
+  return time.toLocaleLowerCase()
+}
+
+const callButtons = document.querySelectorAll('.btn-call')
+
+callButtons.forEach((callBtns)=>{
+  callBtns.addEventListener('click' , (e)=>{
+    e.preventDefault()
+
+    let phoneNumber = e.target.parentElement.parentElement.childNodes[5].innerText
+    let serviceName = e.target.parentElement.parentElement.childNodes[3].childNodes[1].innerText
+    alert(`Calling ${serviceName} on ${phoneNumber}...`)
+
+    let historyLi = document.createElement('li')
+    historyLi.classList.add('call-history-item')
+
+    let historyDivLeft = document.createElement('div')
+    historyDivLeft.classList.add('history-item-left')
+    let serviceTitle = document.createElement('p')
+    let serviceNum = document.createElement('p')
+    serviceTitle.classList.add('history-item-title')
+    serviceNum.classList.add('history-item-number')
+    serviceTitle.innerText = serviceName
+    serviceNum.innerText = phoneNumber
+    historyDivLeft.appendChild(serviceTitle)
+    historyDivLeft.appendChild(serviceNum)
+    historyLi.appendChild(historyDivLeft)
+
+    let historyDivRight = document.createElement('div')
+    historyDivRight.classList.add('history-item-right')
+    let timeText = document.createElement('p')
+    timeText.classList.add('call-history-time')
+    timeText.innerHTML = getCurrentTime()
+    historyDivRight.appendChild(timeText)
+    historyLi.appendChild(historyDivRight)
+
+    const historyList = document.getElementsByClassName('call-history-list')
+    historyList[0].appendChild(historyLi)
+
+  })
+})
